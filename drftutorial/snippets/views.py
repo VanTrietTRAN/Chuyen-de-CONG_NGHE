2585554299d14239ca6c95128ce778bab3f98d00
VanTrietTRAN <work.triettran@gmail.com>
2025-09-16 16:35:33 +0700

    Tutorial 6

--- drftutorial/snippets/views.py
+++ drftutorial/snippets/views.py
@@ -13,6 +13,15 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class SnippetHighlight(generics.GenericAPIView):
     queryset = Snippet.objects.all()
